d0d227ae189efc1836f5f561f830cc3983870f66
i30mb1 <fate.i30mb1@gmail.com>
2017-12-17 17:04:39 +0300

    0.2

--- app/src/main/java/n7/pazz/utils.java
+++ app/src/main/java/n7/pazz/utils.java
@@ -34,47 +34,51 @@ public class utils {
         return bitmap;
     }
     public static void startSpriteAnim(Context context, final ImageView img, String nameSprite, Boolean isLooped, int width, int hight, int duration, int scale) {
-        Bitmap birdBmp = getBitmapFromAssets(context,nameSprite);
-        if (birdBmp != null) {
-            // cut bitmaps from bird to array of bitmaps
-            Bitmap[] bmps;
-            int FRAME_W = width;
-            int FRAME_H = hight;
-            int NB_FRAMES = (birdBmp.getWidth()/width);
-            int FRAME_DURATION = duration; // in ms !
-            int SCALE_FACTOR = scale;
-            int COUNT_X = (birdBmp.getWidth()/width);
-            int COUNT_Y = 1;
+        try {
+            Bitmap birdBmp = getBitmapFromAssets(context, nameSprite);
+            if (birdBmp != null) {
+                // cut bitmaps from bird to array of bitmaps
+                Bitmap[] bmps;
+                int FRAME_W = width;
+                int FRAME_H = hight;
+                int NB_FRAMES = (birdBmp.getWidth() / width);
+                int FRAME_DURATION = duration; // in ms !
+                int SCALE_FACTOR = scale;
+                int COUNT_X = (birdBmp.getWidth() / width);
+                int COUNT_Y = 1;
 
-            bmps = new Bitmap[NB_FRAMES];
-            int currentFrame = 0;
+                bmps = new Bitmap[NB_FRAMES];
+                int currentFrame = 0;
 
-            for (int i = 0; i < COUNT_Y; i++) {
-                for (int j = 0; j < COUNT_X; j++) {
-                    bmps[currentFrame] = Bitmap.createBitmap(birdBmp, FRAME_W * j, FRAME_H * i, FRAME_W, FRAME_H);
-                    // apply scale factor
-                    bmps[currentFrame] = Bitmap.createScaledBitmap(bmps[currentFrame], FRAME_W*SCALE_FACTOR, FRAME_H*SCALE_FACTOR, true);
-                    if (++currentFrame >= NB_FRAMES) {
-                        break;
+                for (int i = 0; i < COUNT_Y; i++) {
+                    for (int j = 0; j < COUNT_X; j++) {
+                        bmps[currentFrame] = Bitmap.createBitmap(birdBmp, FRAME_W * j, FRAME_H * i, FRAME_W, FRAME_H);
+                        // apply scale factor
+                        bmps[currentFrame] = Bitmap.createScaledBitmap(bmps[currentFrame], FRAME_W * SCALE_FACTOR, FRAME_H * SCALE_FACTOR, true);
+                        if (++currentFrame >= NB_FRAMES) {
+                            break;
+                        }
                     }
                 }
-            }
-            // create animation programmatically
-            final AnimationDrawable animation = new AnimationDrawable();
-            animation.setOneShot(isLooped); // repeat animation
+                // create animation programmatically
+                final AnimationDrawable animation = new AnimationDrawable();
+                animation.setOneShot(isLooped); // repeat animation
 
-            for (int i = 0; i < NB_FRAMES; i++) {
-                animation.addFrame(new BitmapDrawable(context.getResources(), bmps[i]), FRAME_DURATION);
-            }
-            // load animation on image
-            img.setImageDrawable(animation);
-            // start animation on image
-            img.post(new Runnable() {
-                @Override
-                public void run() {
-                    animation.start();
+                for (int i = 0; i < NB_FRAMES; i++) {
+                    animation.addFrame(new BitmapDrawable(context.getResources(), bmps[i]), FRAME_DURATION);
                 }
-            });
+                // load animation on image
+                img.setImageDrawable(animation);
+                // start animation on image
+                img.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        animation.start();
+                    }
+                });
+            }
+        } catch (OutOfMemoryError error) {
+            error.printStackTrace();
         }
     }
 }
